ff39d7f5b63cf83d49bf43dd666b84c243d6653c
myersan7 <myersan7@gmail.com>
2016-02-23 16:04:55 -0500

    committ ya

--- Lampyrid_analysis.R
+++ Lampyrid_analysis.R
@@ -16,6 +16,21 @@ lampyrid$DOY<-yday(lampyrid$newdate)
 
 #download weather data from KBS weather station
 weather<-read.table(file="http://lter.kbs.msu.edu/datatables/7.csv",
-                    header=T, sep=",")
-Status API Training Shop Blog About Pricing
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
+                    header=T, sep=",", na.strings="")
+weather$DOY<-yday(weather$date)
+plot(weather$DOY, weather$air_temp_mean)
+plot(weather$DOY, weather$precipitation)
+#calculate the degree day accumulation for the first half of the day 
+#assuming a sine wave structure of temperature over the day
+#use a development threshold of 10c, well, because it's a nice number to work with
+thresh<-10
+alpha<-(weather$air_temp_max-weather$air_temp_min)/2
+if (weather$air_temp_max>thresh) {
+  weather$theta<-asin((10-(weather$air_temp_max+weather$air_temp_min)/2)/alpha)
+}
+else{
+  0
+}
+weather$dd1<-(1/2*pi*((weather$air_temp_max+weather$air_temp_min)/2)-10)*(pi/2)
+
+triangledd<-
